c3074758e60a4670a98f757b8dd3622df2a4e021
joshuacork293@yahoo.com <>
2018-10-24 15:45:41 -0400

    Added hello().

---
+++ src/githubtest3/GitHubTest3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest3;
+
+/**
+ *
+ * @author jrc62
+ */
+public class GitHubTest3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        hello();
+    }
+    public static void hello()
+    {
+        System.out.println("Hello there.");
+    }
+    
+}
